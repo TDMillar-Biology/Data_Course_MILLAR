5b4dff1712227af54d0a695d6332f053a84122ab
Trevor D. Millar <Tmillar32@gmail.com>
2020-09-20 21:44:01 -0600

    Added Chart title in Assignment_4

--- Assignments/Assignment_4/Assignment_4.R
+++ Assignments/Assignment_4/Assignment_4.R
@@ -2,11 +2,11 @@ df <- read.table("../../Data/ITS_mapping.csv", header = TRUE, sep = "\t")
 
 summary(df)
 str(df)
-plot(x=(as.factor(df$Ecosystem)),y=df$Lat, xlab = "Ecosystem", ylab = "Latitude", cex.lab=1, cex.axis=0.4, cex.main=1, cex.sub=1, main = "")     
+plot(x=(as.factor(df$Ecosystem)),y=df$Lat, xlab = "Ecosystem", ylab = "Latitude", cex.lab=1, cex.axis=0.4, cex.main=1, cex.sub=1, main = "Ecosystems by Latitude")     
 
 png(filename = "./silly_boxplot.png")
 par(mar=c(3,3,3,3)+.1)
-plot(x=(as.factor(df$Ecosystem)),y=df$Lat, xlab = "Ecosystem", ylab = "Latitude", cex.lab=1, cex.axis=0.4, cex.main=1, cex.sub=1, main = "")    
+plot(x=(as.factor(df$Ecosystem)),y=df$Lat, xlab = "Ecosystem", ylab = "Latitude", cex.lab=1, cex.axis=0.4, cex.main=1, cex.sub=1, main = "Ecosystems by Latitude")    
 dev.off()
 
 
