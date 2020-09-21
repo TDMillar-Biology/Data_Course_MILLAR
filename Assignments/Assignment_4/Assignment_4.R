b100cb4fd5049bc6109a37cad32093611ab6d90c
Trevor D. Millar <Tmillar32@gmail.com>
2020-09-20 21:47:06 -0600

    Odds and ends with the boxplot

--- Assignments/Assignment_4/Assignment_4.R
+++ Assignments/Assignment_4/Assignment_4.R
@@ -5,7 +5,6 @@ str(df)
 plot(x=(as.factor(df$Ecosystem)),y=df$Lat, xlab = "Ecosystem", ylab = "Latitude", cex.lab=1, cex.axis=0.4, cex.main=1, cex.sub=1, main = "Ecosystems by Latitude")     
 
 png(filename = "./silly_boxplot.png")
-par(mar=c(3,3,3,3)+.1)
 plot(x=(as.factor(df$Ecosystem)),y=df$Lat, xlab = "Ecosystem", ylab = "Latitude", cex.lab=1, cex.axis=0.4, cex.main=1, cex.sub=1, main = "Ecosystems by Latitude")    
 dev.off()
 
