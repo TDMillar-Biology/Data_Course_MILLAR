4fd3984ada07e5ee9085fb48d791890309e56d09
Trevor D. Millar <Tmillar32@gmail.com>
2020-09-20 20:57:44 -0600

    EXAM 1 done

--- Assignments/Assignment_4/Assignment_4.R
+++ Assignments/Assignment_4/Assignment_4.R
@@ -1,10 +1,12 @@
 df <- read.table("../../Data/ITS_mapping.csv", header = TRUE, sep = "\t")
 
-plot(x=(as.factor(df$Ecosystem)),y=df$Lat, xlab = "Ecosystem", ylab = "Latitude", cex.lab=1, cex.axis=0.5, cex.main=1, cex.sub=1)     
+summary(df)
+str(df)
+plot(x=(as.factor(df$Ecosystem)),y=df$Lat, xlab = "Ecosystem", ylab = "Latitude", cex.lab=1, cex.axis=0.4, cex.main=1, cex.sub=1, main = "")     
 
 png(filename = "./silly_boxplot.png")
 par(mar=c(3,3,3,3)+.1)
-plot(x=(as.factor(df$Ecosystem)),y=df$Lat, xlab = "Ecosystem", ylab = "Latitude", cex.lab=1, cex.axis=0.4, cex.main=1, cex.sub=1)    
+plot(x=(as.factor(df$Ecosystem)),y=df$Lat, xlab = "Ecosystem", ylab = "Latitude", cex.lab=1, cex.axis=0.4, cex.main=1, cex.sub=1, main = "")    
 dev.off()
 
 
--- Exam_1/MILLAR_Skills_Test_1.R
+++ Exam_1/MILLAR_Skills_Test_1.R
@@ -1,14 +1,15 @@
 library(tidyverse)
 df <- read.csv("DNA_Conc_by_Extraction_Date.csv")
 head(df)
-## This is q1
+## This is q1 ---
 hist(df$DNA_Concentration_Katy, xlab = "DNA Concentration", main = "Katy's DNA Concentrations")
 hist(df$DNA_Concentration_Ben, xlab = "DNA Concentration", main = "Ben's DNA Concentrations")
-## This is q2 --- Make sure to clean these up before turning them in. 
-png("./MILLAR_Plot1.png")
+
+## This is q2 ---  
+jpeg("./MILLAR_Plot1.jpeg")
 plot(x = (as.factor(df$Year_Collected)), y = df$DNA_Concentration_Katy , xlab = "Year", ylab = "DNA Concentration", main = "Katy's Extractions")
 dev.off()
-png("./MILLAR_Plot2.png")
+jpeg("./MILLAR_Plot2.jpeg")
 plot(x = (as.factor(df$Year_Collected)), y = df$DNA_Concentration_Ben , xlab = "Year", ylab = "DNA Concentration", main = "Ben's Extractions")
 dev.off()
 ## Q3 was added ^^^
@@ -20,10 +21,10 @@ new_vector <- df$DNA_Concentration_Ben / df$DNA_Concentration_Katy
 min_difference <- min(new_vector)
 index_difference <- which(min_difference == new_vector)
 lowest_difference_year <- df$Year_Collected[index_difference]
-
+print(lowest_difference_year)
 ##Q5
 subset_df_downstairs <- filter(df,Lab %in% "Downstairs")
-png("./Ben_DNA_over_time.jpg")
+jpeg("./Ben_DNA_over_time.jpeg")
 plot(x = as.POSIXct(subset_df_downstairs$Date_Collected), y = subset_df_downstairs$DNA_Concentration_Ben, xlab = "Date_Sample_Collection",ylab = "DNA_Concentration_Ben")
 dev.off()
 
