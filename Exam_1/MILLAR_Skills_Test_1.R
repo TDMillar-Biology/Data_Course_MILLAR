1dea2742a2c3fb0a17a0d64fae707245bcbb2562
Trevor D. Millar <Tmillar32@gmail.com>
2020-09-20 18:35:16 -0600

    Assignment 4 Completion

--- Exam_1/MILLAR_Skills_Test_1.R
+++ Exam_1/MILLAR_Skills_Test_1.R
@@ -1,4 +1,4 @@
-
+library(tidyverse)
 df <- read.csv("DNA_Conc_by_Extraction_Date.csv")
 head(df)
 ## This is q1
@@ -14,11 +14,17 @@ dev.off()
 ## Q3 was added ^^^
  
 ##Q4 visualization
-
+plot(x = df$DNA_Concentration_Ben, y = df$DNA_Concentration_Katy)
 ##Q4 real question
+new_vector <- df$DNA_Concentration_Ben / df$DNA_Concentration_Katy
+min_difference <- min(new_vector)
+index_difference <- which(min_difference == new_vector)
+lowest_difference_year <- df$Year_Collected[index_difference]
 
 ##Q5
-
-
+subset_df_downstairs <- filter(df,Lab %in% "Downstairs")
+png("./Ben_DNA_over_time.jpg")
+plot(x = as.POSIXct(subset_df_downstairs$Date_Collected), y = subset_df_downstairs$DNA_Concentration_Ben, xlab = "Date_Sample_Collection",ylab = "DNA_Concentration_Ben")
+dev.off()
 
 
